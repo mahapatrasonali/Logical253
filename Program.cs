73af8f03fad2312ea968287b91f44b13b3828083
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-01-30 12:15:03 +0530

    Logical Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LogicalProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // FibonacciSeries.DisplayFibonacciSeries();
+            // PerfectNumber.FindNumbers();
+            // ReverseNumber.Display();
+            //PrimeNumber.CheckPrime();
+            //StopWatch.Simulate();
+            CouponNumbers.Couponnumber();
+        }
+    }
+}
